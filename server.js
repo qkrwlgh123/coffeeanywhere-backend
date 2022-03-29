e5311b96962f57b963a4c9ddd68d8cfa911dea32
qkrwlgh123 <jiho1024@naver.com>
2022-03-29 19:17:18 +0900

    Backend setup

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+import { ApolloServer } from 'apollo-server';
+import schema from './schema';
+
+const PORT = process.env.PORT;
+const server = new ApolloServer({
+  schema,
+});
+
+server
+  .listen(PORT)
+  .then(() =>
+    console.log(`Jiho's Server is running on http://localhost:${PORT}!!`)
+  );
