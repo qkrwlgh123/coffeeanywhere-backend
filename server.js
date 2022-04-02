3b7e66419bb36813efea6601b771e4f04e0318d9
qkrwlgh123 <jiho1024@naver.com>
2022-04-02 12:15:58 +0900

    서버 express로 수정 후, apollo-server-express로 변경

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import logger from 'morgan';
+import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './schema';
 import { getUser } from './users/users.utils';
 
 const PORT = process.env.PORT;
 
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   context: async ({ req }) => {
@@ -15,8 +17,12 @@ const server = new ApolloServer({
   },
 });
 
-server
-  .listen(PORT)
-  .then(() =>
-    console.log(`Jiho's Server is running on http://localhost:${PORT}!!`)
-  );
+const app = express();
+app.use(logger('tiny'));
+app.use('/static', express.static('uploads'));
+
+apollo.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+  console.log(`Jiho's Server is running on http://localhost:${PORT}/graphql`);
+});
