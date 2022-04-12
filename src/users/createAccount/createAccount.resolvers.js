6cb700b2d4645c1dc1619e803c2f153e4ef3dc3d
qkrwlgh123 <jiho1024@naver.com>
2022-04-12 22:09:22 +0900

    Modify signup code

--- src/users/createAccount/createAccount.resolvers.js
+++ src/users/createAccount/createAccount.resolvers.js
@@ -26,29 +26,29 @@ export default {
             ok: false,
             error: 'This username is already exist.',
           };
-        }
-        // hash the password
-        const uglyPassword = await bcrypt.hash(password, 10);
-        // save and return the user
-        const createdUser = await client.user.create({
-          data: {
-            name,
-            username,
-            email,
-            githubUsername,
-            location,
-            password: uglyPassword,
-          },
-        });
-        if (createdUser.name) {
-          return {
-            ok: true,
-          };
         } else {
-          return {
-            ok: false,
-            error: 'Could not create a user.',
-          };
+          const uglyPassword = await bcrypt.hash(password, 10);
+          // save and return the user
+          const createdUser = await client.user.create({
+            data: {
+              name,
+              username,
+              email,
+              githubUsername,
+              location,
+              password: uglyPassword,
+            },
+          });
+          if (createdUser.name) {
+            return {
+              ok: true,
+            };
+          } else {
+            return {
+              ok: false,
+              error: 'Could not create a user.',
+            };
+          }
         }
       } catch (e) {
         return e;
